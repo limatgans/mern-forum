bc6acefc5d3cfefdd2f991376c26995a36b9969d
Ganesh Mohanraj <gans@anton.thefeathers.in>
2021-05-24 00:50:25 +0530

    [feat] SignUp

--- frontend/mern-forum/src/Main.jsx
+++ frontend/mern-forum/src/Main.jsx
@@ -1,11 +1,13 @@
 import Login from "./pages/Login.jsx";
 import {Switch,Route} from 'react-router-dom';
+import SignUp from "./pages/Signup.jsx";
 
 
 export default function Main () {
 	return (
 		<Switch>
 			<Route exact path="/" component={Login} />
+			<Route exact path="/signup" component={SignUp} />
 		</Switch>
 	)
 }
